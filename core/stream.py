92f0bc85577aedcf9aaeeb2490478a1382f9b43d
vivekaprasad <96980843+vivekaprasad@users.noreply.github.com>
2023-01-10 02:43:58 +0530

    Update stream.py

--- core/stream.py
+++ core/stream.py
@@ -117,11 +117,6 @@ async def start_stream(song: Song, lang):
         % (
             song.title,
             song.source,
-            song.duration,
-            song.request_msg.chat.id,
-            song.requested_by.mention
-            if song.requested_by
-            else song.request_msg.sender_chat.title,
         ),
         quote=False,
     )
